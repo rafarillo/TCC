807431c73ec56616d5747f3c29c97519bff2b6f0
rafarillo <rafaelgtvc@gmail.com>
2022-05-01 02:55:03 -0300

    Primeira Fase do Algoritmo

--- geocomp-py-framework/geocomp/polygonintersections/greiner.py
+++ geocomp-py-framework/geocomp/polygonintersections/greiner.py
@@ -16,21 +16,26 @@ class LinkedList:
         self.intersect = False
         self.entryExit = False
         self.neighbor = False
-        self.alpha = 1.0
+        self.alpha = 0.0
 
     def insert(self, new):
         if self.next is None:
             self.next = new
             new.prev = self
+            return new
         else:
             p = self
-            while p is not None and p.next.alpha < new.alpha:
-                p = p.next
+            while p.next and p.next.intersect:
+                if p.next.alpha < new.alpha:
+                    p = p.next
+                else:
+                    break
             aux = p.next
             p.next = new
             new.prev = p
             new.next = aux
-            aux.prev = new
+            if aux: aux.prev = new
+            return new
 
     def create_intersect_node(self, A, B):
         vertex = CalculateIntersectPoint(A, B)
@@ -40,7 +45,7 @@ class LinkedList:
         d1 = primitive.dist2(A.init, A.to)
         d2 = primitive.dist2(A.init, new.vertex)
         new.alpha = d2/d1
-        self.insert(new)
+        return self.insert(new)
 
 def ArrayToList(P):
     first = LinkedList(P)
@@ -67,41 +72,55 @@ def CalculateIntersectPoint(A, B):
     return Point(x, y)
 
 
-def Intersect(P1, P2):
+def Intersect(P1, P2, l):
     p1 = P1
-    intersect = False
 
     while p1 is not None:
         p2 = P2
-        if p1.next is not None: A = Segment(p1.vertex, p1.next.vertex)
+        nextP1 = p1.next
+        if nextP1 is not None: A = Segment(p1.vertex, nextP1.vertex)
         else: A = Segment(p1.vertex, P1.vertex) #corner case
         A.hilight(color_line='blue')
-        while not p1.intersect and p2 is not None:
-            if p2.next is not None: B = Segment(p2.vertex, p2.next.vertex) 
+        while p2 is not None:
+            nextP2 = p2.next
+            while nextP2 and nextP2.intersect: nextP2 = nextP2.next 
+            if nextP2 is not None: B = Segment(p2.vertex, nextP2.vertex) 
             else: B = Segment(p2.vertex, P2.vertex) #corner case
             B.hilight(color_line='green')
-            if not p2.intersect and A.intersects(B):
-                p1.create_intersect_node(A, B)
-                p2.create_intersect_node(B, A)
-                p1.next.neighbor = p2.next
-                p2.next.neighbor = p1.next
+            if A.intersects(B):
+                new1 = p1.create_intersect_node(A, B)
+                new2 = p2.create_intersect_node(B, A)
+                new1.neighbor = new2
+                new2.neighbor = new1
             control.freeze_update ()
             control.thaw_update() 
             control.update()
             control.sleep()
             B.hilight(color_line='red')
-            p2 = p2.next
+            p1.vertex.hilight(color='red')
+            p2.vertex.hilight(color='red')
+            p2 = nextP2
         A.hilight(color_line='red')
-        p1 = p1.next
+        p1 = nextP1
 
 
 
 def Greiner(l):
     P1 = ArrayToList(l[0].pts)
     P2 = ArrayToList(l[1].pts)
-    Intersect(P1, P2)
+    Intersect(P1, P2, l)
     #Teste para verificar a ordem dos ponto
     p1 = P1
+    while p1 is not None:
+        p1.vertex.hilight(color='white')
+        print('vertice = ', p1.vertex, 'alpha = ', p1.alpha)
+        control.freeze_update ()
+        control.thaw_update() 
+        control.update()
+        control.sleep()
+        p1.vertex.hilight(color='red')
+        p1 = p1.next
+    p1 = P2
     while p1 is not None:
         p1.vertex.hilight(color='white')
         print('vertice = ', p1.vertex, 'alpha = ', p1.alpha)
